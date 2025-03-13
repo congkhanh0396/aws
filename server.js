e51fb9eaaad450ab3eeca83ade90eb6d3d01ca67
dominic <dominic.tran@beyondedge.co>
2025-03-13 10:33:05 +0700

    first step to deploy

--- server.js
+++ server.js
@@ -6,6 +6,6 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+server.listen(port, '0.0.0.0', () => {
+  console.log(`Server running at http://0.0.0.0:${port}/`);
 });
