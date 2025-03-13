677894f016c265c39dce5ca6df8b063cb88b212b
dominic <dominic.tran@beyondedge.co>
2025-03-13 11:00:49 +0700

    Add GitHub Actions for auto-deploy

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!, server is running by express js')
+  res.send('Hello World!, server is running by express js and update cicd')
 })
 
 app.listen(port, () => {
